be4d7c5b87845b56e50cc54251f9ccc739b54d9b
lukasGrig <lukasgri06@gmail.com>
2024-11-12 13:06:03 +0000

    meira stuff

--- club_scout/app/logIn-SignUp/page.tsx
+++ club_scout/app/logIn-SignUp/page.tsx
@@ -1,8 +1,9 @@
 import { sign_up,sign_in } from "@/app/library/actions" //þarf ekki .tsx
+import { redirect } from "next/navigation"
 
 export function Form(props :{type: string}){
   const {type} = props
-  if (type==="log in"){
+  if (type==="log_in"){
     return(
       <form style={{margin: "auto",width: "50%", textAlign: "center"}} action={sign_in}>
         
@@ -17,7 +18,7 @@ export function Form(props :{type: string}){
       </form>
     )
   }
-  else {
+  else if (type==="sign_up"){
     return(
       <form style={{margin: "auto",width: "50%", textAlign: "center"}} action={sign_up}>
 
@@ -35,15 +36,17 @@ export function Form(props :{type: string}){
       </form>
     )
   }
+  else{
+    redirect('/../..')
+  }
   
 }
 // TEST TEST
-export default function Page(){//make it so it takes in a prop and uses it to define the Form
+export default function Page(props: {type:string}){//make it so it takes in a prop and uses it to define the Form
   return (
     <div>
       <h1>THIS IS A TEST</h1>
-      <Form type="log in" />
-      <Form type=""/>
+      <Form type={props.type} />
     </div>
 )
 }
