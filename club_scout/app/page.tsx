c594c58bb9c18cb1b08e0b57ff60989a29d1c580
agijens <agijens@gmail.com>
2024-11-14 11:57:09 +0000

    laga

--- club_scout/app/page.tsx
+++ club_scout/app/page.tsx
@@ -6,7 +6,10 @@ import styles from "./page.module.css"  // Adjust path as necessary
 export default function Page(){
     return (
         <div>
-            <Link href={"/logIn-SignUp/log_in"}>Login</Link>
+            <Link href="/logIn-SignUp/log_in" className={styles.link}>Login</Link>
+            <Link href="/About" className={styles.link}>About</Link>
+            <Link href="/Club" className={styles.link}>Club</Link>
+            <Link href="/profile" className={styles.link}>Profile</Link>
             <br />
             <Link href={"/logIn-SignUp/sign_up"}>SignUp</Link>
         </div>
