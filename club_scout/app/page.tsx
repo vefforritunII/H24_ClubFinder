282ae7510198455d7d0e141527978456bd25de06
agijens <agijens@gmail.com>
2024-11-15 09:10:07 +0000

    component folder-Header

--- club_scout/app/page.tsx
+++ club_scout/app/page.tsx
@@ -2,16 +2,18 @@
 
 import Link from "next/link"
 import styles from "./page.module.css"  // Adjust path as necessary
+import Header from './components/Header'; // Adjust the path as needed
 
 export default function Page(){
     return (
         <div>
+            <Header />
             <Link href="/logIn-SignUp/log_in" className={styles.link}>Login</Link>
             <Link href="/About" className={styles.link}>About</Link>
             <Link href="/Club" className={styles.link}>Club</Link>
             <Link href="/profile" className={styles.link}>Profile</Link>
             <br />
-            <Link href={"/logIn-SignUp/sign_up"}>SignUp</Link>
+            <Link href="/logIn-SignUp/sign_up"className={styles.link}>SignUp</Link>
         </div>
     )
 }
