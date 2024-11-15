9d8835b924b508d719370aebf846a188ce627e04
lukasGrig <lukasgri06@gmail.com>
2024-11-15 09:31:13 +0000

    bætt við link til club

--- club_scout/app/components/profileMemberClubs.tsx
+++ club_scout/app/components/profileMemberClubs.tsx
@@ -1,10 +1,11 @@
+import Link from "next/link";
 
 // return-a club með nafn,description og logo-ið og id í key svo hún mun ekki væla
 export default function memberOfClubs(name: string,descreption:string,logo:string,id:string){
     return(
         <div key={id}>
             <img src={logo} alt={logo} />
-            <h3>{name}</h3>
+            <Link href={"/Club/"+id}>{name}</Link>
             <p>{descreption}</p>
         </div>
     )
