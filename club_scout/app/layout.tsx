88b3d56503d1825b385e2d6954e2a7b24d3216c3
agijens <agijens@gmail.com>
2024-11-18 09:20:35 +0000

    Removaði header og er bara með sidebar

--- club_scout/app/layout.tsx
+++ club_scout/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import Header from "./components/Header"; // Adjust the path based on your folder structure
+import Header from "./components/Sidebar"; // Adjust the path based on your folder structure
 import "./globals.css";
 
 const geistSans = localFont({
