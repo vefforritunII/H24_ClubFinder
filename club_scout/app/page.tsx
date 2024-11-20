4af2971e3aac31713ab9be58d22cece780046564
agijens <agijens@gmail.com>
2024-11-20 23:07:03 +0000

    bara breyta comment og remove-a profile link úr home

--- club_scout/app/page.tsx
+++ club_scout/app/page.tsx
@@ -8,7 +8,6 @@ export default function Page(){
     return (
         <><h1>Home Page</h1><div>
             <Link href="/logIn-SignUp/log_in" className={styles.link}>Login</Link>
-            <Link href="/profile" className={styles.link}>Profile</Link>
             <br />
             <Link href="/logIn-SignUp/sign_up" className={styles.link}>SignUp</Link>
         </div></>
