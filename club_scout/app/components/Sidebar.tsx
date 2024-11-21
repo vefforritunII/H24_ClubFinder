761425a6b5e385315ffca27f96d0933c391abe12
agijens <agijens@gmail.com>
2024-11-21 01:10:38 +0000

    addaði hamburger button i mobile og tablets sem opnar og lokar sidebar

--- club_scout/app/components/Sidebar.tsx
+++ club_scout/app/components/Sidebar.tsx
@@ -1,26 +1,43 @@
 "use client"; // Mark this file as a client-side component
 
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Sidebar.module.css'; // Import the CSS module
 import Link from 'next/link';
 
 const StaticSidebar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsOpen(prevState => !prevState);
+  };
+
   return (
-    <div className={styles.sidebar}>
-      <ul className={styles.sidebarLinks}>
-        <li><h1 className={styles.h1}>Clubscout</h1></li>
-        <li><Link href="/" className={styles.link}>Home</Link></li>
-        <li><Link href="/Club" className={styles.link}>Discover</Link></li>
-        <li><Link href="" className={styles.link}>Your Clubs</Link></li>
-        <li><Link href="" className={styles.link}>Messages</Link></li>
-        <li><Link href="/About" className={styles.link}>About</Link></li>
-      </ul>
-      {/* Profile Link at the bottom */}
-      <div className={styles.profileContainer}>
-        <Link href="/profile/User" className={styles.link}>Profile</Link>
+    <div>
+      {/* Hamburger Menu Icon */}
+      <div className={styles.hamburger} onClick={toggleSidebar}>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+
+      {/* Sidebar */}
+      <div className={`${styles.sidebar} ${isOpen ? styles.open : ''}`}>
+        <ul className={styles.sidebarLinks}>
+          <li><h1 className={styles.h1}>Clubscout</h1></li>
+          <li><Link href="/" className={styles.link}>Home</Link></li>
+          <li><Link href="/Club" className={styles.link}>Discover</Link></li>
+          <li><Link href="" className={styles.link}>Your Clubs</Link></li>
+          <li><Link href="" className={styles.link}>Messages</Link></li>
+          <li><Link href="/About" className={styles.link}>About</Link></li>
+        </ul>
+        {/* Profile Link at the bottom */}
+        <div className={styles.profileContainer}>
+          <Link href="/profile/User" className={styles.link}>Profile</Link>
+        </div>
       </div>
     </div>
   );
 };
 
 export default StaticSidebar;
+
