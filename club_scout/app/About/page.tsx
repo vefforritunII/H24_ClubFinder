bb1f373cbbffaa54b56b4d8a4920f6922bce29f0
agijens <agijens@gmail.com>
2024-11-26 12:51:06 +0000

    bætti eitthvað smá orð inn í about us

--- club_scout/app/About/page.tsx
+++ club_scout/app/About/page.tsx
@@ -1,6 +1,7 @@
 export default function Page() {
     return (
-        <h1>about us</h1>
+        <><h1>about us</h1><p>Við bjuggum til þennan fyrirtæki svo að fólk mun vilja tilheyra samfélagi sem hefur sama áhuga og notandinn á ákveðnum hlutum eins og tölvuleikjum, íþróttum, borðspilum o.s.frv. Notendur finna sér klúbba til að ganga í og ​​mæta síðan í reglulega dagskrá eða viðburð
+            i til að taka þátt í þeirri ákveðnu starfsemi sem klúbburinn snýst um og vonandi eignast nýja vini í leiðinni.</p></>
     );
 }
 
