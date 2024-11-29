b5c9c7c4015e11f8e7bef103fda4b7976016f0f7
lukasGrig <lukasgri06@gmail.com>
2024-11-29 17:48:57 +0000

    quick fix í signup function línu 21

--- club_scout/app/library/actions.tsx
+++ club_scout/app/library/actions.tsx
@@ -17,7 +17,8 @@ export async function sign_up(formdata: FormData){
     }
     
     // ká ef password og passwordCheck eru eins og svo búa til nýja profile
-    if (signUpData.password === signUpData.passwordCheck && String(signUpData.username).length >= 8) {
+    // BÆTT VIÐ LEIÐ TIL AÐ KÁ EF USERNAME ER UNIQUE (supabase eða í if statement)
+    if (signUpData.password === signUpData.passwordCheck && String(signUpData.password).length >= 8) {
 
         const { error } = await supabase
         .from('profiles')
