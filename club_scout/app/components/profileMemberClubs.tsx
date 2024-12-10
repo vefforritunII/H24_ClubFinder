b79e2fc2fa0c021493f7335025799225b1bc3279
Neon <mateuszko22@tskoli.is>
2024-12-10 13:03:57 +0000

    revert some edits

--- club_scout/app/components/profileMemberClubs.tsx
+++ club_scout/app/components/profileMemberClubs.tsx
@@ -1,29 +1,24 @@
 import Link from "next/link";
-import styles from "./profileMemberClubs.module.css";
 
 // return-a club með nafn,description og logo-ið og id í key svo hún mun ekki væla
 export default function memberOfClubs(name: string,descreption:string,logo:string,id:string,owner:boolean,username:string = "none"){
     if (owner === false){
         return(
-            <Link className={styles.club_element} href={"/Club/"+id} key={id}>
-                <img src={logo} alt={logo}/>
-                <div className={styles.club_details}>
-                    <p className={styles.club_title}>{name}</p>
-                    <p className={styles.club_description}>{descreption}</p>
-                </div>
-            </Link>
+            <div key={id}>
+                <img src={logo} alt={logo} />
+                <Link href={"/Club/"+id}>{name}</Link>
+                <p>{descreption}</p>
+            </div>
         )
     }
     else if (owner === true){
         return(
-            <Link className={styles.club_element} href={"/Club/"+id} key={id}>
+            <div key={id}>
                 <img src={logo} alt={logo} />
-                <div className={styles.club_details}>
-                    <p className={styles.club_title}>{name}</p>
-                    <p className={styles.club_description}>{descreption}</p>
-                    <Link href={"/profile/"+username+"/"+id}>Change!</Link>
-                </div>
-            </Link>
+                <Link href={"/Club/"+id}>{name}</Link>
+                <p>{descreption}</p>
+                <Link href={"/profile/"+username+"/"+id}>Change!</Link>
+            </div>
         )
     }
 
