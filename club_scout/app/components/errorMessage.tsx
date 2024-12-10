4d9a0279cac6c9c7db5526c3ecd824b2cf275cbd
lukasGrig <lukasgri06@gmail.com>
2024-12-10 14:30:53 +0000

    quci fix error msg

--- club_scout/app/components/errorMessage.tsx
+++ club_scout/app/components/errorMessage.tsx
@@ -1,7 +1,7 @@
 export default function errorMessage(errorMessage:string){
     return (
         <div style={{textAlign:"center"}}>
-            <p style={{display: "inline-block",fontSize: "85%",border:"red 1px solid",borderRadius:"20px",padding: "20px",marginTop: "10px",backgroundColor: "black"}}>ERROR: {errorMessage}</p>
+            <p style={{display: "inline-block",fontSize: "85%",color:"white",border:"red 1px solid",borderRadius:"20px",padding: "20px",marginTop: "10px",backgroundColor: "black"}}>ERROR: {errorMessage}</p>
         </div>
     )
     
